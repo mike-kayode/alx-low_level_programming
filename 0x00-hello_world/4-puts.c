589846bc5810c07825f7e1d920cb950e90b492f3
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-16 13:10:03 -0800

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/* main = entry point */
-/* return = 0 */
+
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 
 int main() {
 	puts("\"Programming is like building a multilingual puzzle");
