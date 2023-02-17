6bb2a9a81353b6ee27b2b5ae5e8cd9969a379bfb
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-17 07:04:03 -0800

    101 Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
-#include <unistd>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - Entry point
+ *Return: Always 1
+ *
+ */
 
 int main(void)
 {
-	char str[] ="and that piece of art is useful" - Dora Korpar, 2015-10-19,\n",
-	     write(STDERR_FILENO, str, sizeof(str)-1);
-	return 1;
-}
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}}
