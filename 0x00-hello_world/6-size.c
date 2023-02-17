6a680d0ce8f5ba57e64d3bd06e144d11ae3fecdb
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-17 06:21:56 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,14 @@
  */
 
 int main(void) 
-{   printf("The size of char is %ld bytes.\n", sizeof(char));
-    printf("The size of short is %ld bytes.\n", sizeof(short));
-    printf("The size of int is %ld bytes.\n", sizeof(int));
-    printf("The size of long is %ld bytes.\n", sizeof(long));
-    printf("The size of long long is %ld bytes.\n", sizeof(long long));
-    printf("The size of float is %ld bytes.\n", sizeof(float));
-    printf("The size of double is %ld bytes.\n", sizeof(double));
-    printf("The size of long double is %ld bytes.\n", sizeof(long double));
+{   printf("The size of char is %zu bytes.\n", sizeof(char));
+    printf("The size of short is %zu bytes.\n", sizeof(short));
+    printf("The size of int is %zu bytes.\n", sizeof(int));
+    printf("The size of long is %zu bytes.\n", sizeof(long));
+    printf("The size of long long is %zu bytes.\n", sizeof(long long));
+    printf("The size of float is %zu bytes.\n", sizeof(float));
+    printf("The size of double is %zu bytes.\n", sizeof(double));
+    printf("The size of long double is %zu bytes.\n", sizeof(long double));
 
     return(0);
 }
