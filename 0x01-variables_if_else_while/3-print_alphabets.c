8b0e7ea79ab05fa3a6cfcc7ead7c4ebaba733f0f
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-19 13:47:38 -0800

    Alphabel

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,20 @@
 int main(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 {
 	putchar(c);
 	c++;
 }
         c = 'A';
+
 	while (c <= 'Z')
 {
 	putchar(c);
 	c++;
 }
         putchar('\n');
+
         return (0);
 }
