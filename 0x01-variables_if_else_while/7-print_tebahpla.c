717f855975c5c6dd3bd92d648aef31a51e419201
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-19 13:05:30 -0800

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i = 'a';
+        while (i <= 'z')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
