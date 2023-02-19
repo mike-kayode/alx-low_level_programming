cf67469f06d93d77c58382d22aea619f5754d8e4
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-19 09:15:33 -0800

    Alphabelth

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+        c = 'A';
+	while (c <= 'Z')
+{
+	putchar(c);
+	c++;
+}
+        putchar('\n');
+        return (0);
+}
