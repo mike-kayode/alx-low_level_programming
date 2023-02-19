ff708caaffe6329b05a8cd1a2a09f23ea654ed0b
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-19 11:22:21 -0800

    NuMBER

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x = 0;
+	while (x < 10)
+{
+	putchar(x + '0');
+	x++;
+}
+putchar('\n');
+return (0);
+}
