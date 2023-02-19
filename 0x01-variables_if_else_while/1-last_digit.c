ca6a7e61ae1b2b78b3db29eafd74c9c768c64b2d
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-19 07:01:48 -0800

    greater

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 
 	if ((n % 5) > 5)
         {
-                printf("Last digit of %d is %d and is greater than 5/n", n (n % 10));
+                printf("Last digit of %d is greater than 5/n", (n % 10));
         }
 
 
