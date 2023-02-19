4d4fd767476f4119e011a19a738585de9797866c
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-19 08:31:53 -0800

    Alphabelt

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *  
+ * main - Entry point 
  * Return: Always 0
  */
 
-int main()
+int main(void)
 {
 	char c = 'a';
-	
 	while (c <= 'z') 
 	{
 		putchar(c);
 
 		c++;
 	}
-
 	putchar('\n');
 	return 0;
 }
