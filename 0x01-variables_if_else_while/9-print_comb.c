234a29b10249df689ba2f7c998510416be6f11a6
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-20 12:50:09 -0800

    Alphabelt

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,14 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-       {
-        putchar(i + '0');
-        if (i != 9)
-       {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-
-    return (0);
+	{
+		putchar(i+ '0');
+		if (i != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
 }
