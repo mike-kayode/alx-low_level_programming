aecc1f1aa7f45bb47ecf7235eb558f9722496b2e
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-22 12:53:34 -0800

    Alphabelt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main- Entry point
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	putchar(");
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,5 +17,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
