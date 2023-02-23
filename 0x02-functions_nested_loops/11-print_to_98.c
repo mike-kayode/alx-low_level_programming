7b1f3373da813a7d177b78e722cd0d0d423b0c86
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-23 13:06:43 -0800

    Nested loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,16 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf ("%d, ", n);
+		printf("%d, ", n);
 		n++;
 	}
 	while (n > 98)
 	{
-		printf ("%d,", n);
+		printf("%d,", n);
 		n--;
 	}
 	if  (n == 98)
-		printf ("%d", n);
+		printf("%d", n);
 
 	printf ("\n");
 }
