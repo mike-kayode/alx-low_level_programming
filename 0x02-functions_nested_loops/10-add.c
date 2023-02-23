59f3fb91e62207671e7ada672f7c84fd5dbf479b
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-23 12:21:01 -0800

    Nested loop

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @x: two integer arguments
+ * @y: two integer arguments
+ * Return: sum
+ */
+
+int add(int x, int y)
+{
+	int sum = x + y;
+	return sum;
+}
+
