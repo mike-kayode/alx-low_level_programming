b569f6282962f9c7962025136e199491b1a84ebb
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-23 18:53:03 -0800

    Nested loop

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mut - multipilies of two integers
+ * @a: first integer
+ * @b: second inter
+ * Return: product of a and b
+ */
+
+int mut(int a, int b)
+{
+	return (a * b);
+}
