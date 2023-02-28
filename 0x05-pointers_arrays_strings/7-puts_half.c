9984e0c810737c77cff0b1705c1a048b776b8c5d
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-28 13:46:49 -0800

    Arays

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,22 +7,19 @@
 
 void puts_half(char *str)
 {
-	int len = _strlen(str);
-	int i;
+	int a, n, length;
 
-	if (len % 2 == 0)
-	{
-		for (i = len / 2; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
-	else
-	{
-		for (i = (len - 1) / 2 + 1; i < len; i++)
-		{
-			_putchar(str[i]);
-		}
-	}
+	length = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		length++;
+	n = (length / 2);
+
+	if ((length % 2) == 1)
+		n = ((length + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+
 	_putchar('\n');
 }
