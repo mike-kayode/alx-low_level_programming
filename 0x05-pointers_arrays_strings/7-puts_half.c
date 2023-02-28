ad55e10f930c5c0aa6d30f3defd19fd9193358f4
mike-kayode <oluwakayodebright22@gmail.com>
2023-02-27 20:40:11 -0800

    Pointer and Array

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of a string
+ * @str: pointer to a tring
+ */
+
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int i;
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (len - 1) / 2 + 1; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
