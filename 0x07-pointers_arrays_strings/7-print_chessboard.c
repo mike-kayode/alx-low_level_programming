2f5b0de9dfd616513793035d26929a8fa6c613c8
mike-kayode <oluwakayodebright22@gmail.com>
2023-03-06 12:26:56 -0800

    Pointers, Arrays and Strings

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: character variable
+ * Return: Success
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			printf("%c ", a[row][col]);
+		}
+		printf("\n");
+	}
+}
