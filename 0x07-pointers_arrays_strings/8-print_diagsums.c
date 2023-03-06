a7c3d15518dc863a5f977b9b6e5df5101c6cd359
mike-kayode <oluwakayodebright22@gmail.com>
2023-03-06 12:52:15 -0800

    Pointers, Arrays and Strings

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square
+ * @a: interger variable
+ * @size: size of square
+ * Return: Success
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0, sum2 = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - i - 1)];
+	}
+	printf("%d\n", sum1);
+	printf("%d\n", sum2);
+}
