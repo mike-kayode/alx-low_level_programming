23a5c4bec135ac29b354fa8eaabfd05b4c4a7b3d
mike-kayode <oluwakayodebright22@gmail.com>
2023-03-08 14:50:16 -0800

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,28 +6,27 @@
  * Return: Success
  */
 
+int find_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ * Return: natural square root, or -1 if not natural root
+ */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	else
-	{
-		int i = 1;
 
-		while (i * i <= n)
-		{
-			if (i * i == n)
-			{
-				return (i);
-			}
-			i++;
-		}
-		return (-1);
-		}
+	return (find_root(n, 0));
 }
