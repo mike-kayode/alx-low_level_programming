90d0d74bf0fec6794490fe2d2f639409971ded8d
mike-kayode <oluwakayodebright22@gmail.com>
2023-03-15 13:45:43 -0700

    Malloc project

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 /**
- * void free_grid - function that frees a 2 dimensional grid
- * @grid: integer arrays 
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: integer arrays
  * @height: second integer
  * Return: sucess
  */
@@ -11,30 +11,10 @@
 void free_grid(int **grid, int height)
 {
 	int i;
-	int j;
-	int **grid;
 
-	if (width <- 0 || height < = 0)
-	{
-		return (NULL);
-	}
-	grid = (int **)malloc(height * sizeof(int *));
-	if (grid == NULL)
-	{
-		return (NULL);
-	}
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)calloc(width, sizeof(int));
-		if (grid[i] == NULL)
-		{
-			for(j = 0; j < i; j++)
-			{
-				free(grid[j]);
-			}
-			free(grid);
-			return (NULL);
-		}
+		free(grid[i]);
 	}
-	return (grid);
+	free(grid);
 }
