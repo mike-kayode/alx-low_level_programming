b9b34191aef76d240ae245db58874dd172a7b258
mike-kayode <oluwakayodebright22@gmail.com>
2023-03-22 21:09:55 -0700

    Functions, pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that print name
+ * @name: character strind
+ * @f: pointer
+ * Return: Success
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if(name != NULL && f != NULL)
+	f(name);
+}
