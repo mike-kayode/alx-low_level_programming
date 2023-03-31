4ced97c72e118dcedad45327d30cbab305c0de13
mike-kayode <oluwakayodebright22@gmail.com>
2023-03-31 15:53:50 -0700

    Lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,11 +16,11 @@ size_t print_list(const list_t *h)
 	{
 		if (current->str != NULL)
 		{
-			printf("[%d] %s\n", current->len, current->str);
+			printf("[0] (nill)\n");
 		}
 		else
 		{
-			printf("[0] (nill)\n");
+			printf("[%d] %s\n", current->len, current->str);
 		}
 		current = current->next;
 		count++;
