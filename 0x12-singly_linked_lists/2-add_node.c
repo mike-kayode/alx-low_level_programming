50d825b8fb2f8db91ae16a2324384ddc2f6037af
mike-kayode <oluwakayodebright22@gmail.com>
2023-03-31 16:29:21 -0700

    Lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,3 @@
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
@@ -10,21 +7,36 @@
  * Return: address of the element
  */
 
+int _strlen (const char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++);
+	return (len);
+}
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
 
+	if (str == NULL)
+		return (NULL);
+	if (strdup(str) == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
+
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->next = *head;
+	new_node->len = _strlen(str);
+
+	if (head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
 	*head = new_node;
+
 	return (new_node);
 }
