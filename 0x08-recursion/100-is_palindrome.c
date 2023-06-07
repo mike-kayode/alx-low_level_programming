8b54c86f2ca33c805a09f7fa61c6a679912f1a68
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-07 15:39:59 -0700

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * is_palindrome - function that return 1
+ * @s: character string
+ * Return: Return 0, 1 if a string is palindrome
+ */
+
 int is_palidrome(char *s)
 {
 	int len = strlen(s);
