6425373d7ed6d56670653c704e9a30b76f8f0dc6
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-07 13:42:45 -0700

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,18 +1,15 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _puts_recursion - Function that print a string
- * @s: character string
- * Return: Always success
- */
+#include <stdio.h>
+#include <unistd.h>
 
-void _puts_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		putchar('\n');
-		return;
-	}
-	putchar(*s);
-	_puts_recursion(s + 1);
-}
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
