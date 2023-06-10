dcf8690d3e11702b0b1450e555dcc193cc9e1898
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-09 19:45:11 -0700

    ARGV

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/*
+ * main - Function that add two positive number
+ * @argc: first interger
+ * @argv: Sstring of arrays
+ * Return: success
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int num;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		char *number = argv[i];
+		int j = 0;
+
+		while (number[j] != '\0')
+		{
+			if (!isdigit(number[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		num = atoi(number);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
