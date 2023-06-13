f4e2979cb9d49f523c33cf374c084852f8b913e9
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-13 14:55:50 -0700

    Malloc project

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,21 +13,28 @@ char *str_concat(char *s1, char *s2)
 	char *result;
 	size_t len1;
 	size_t len2;
+	size_t total;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	len1 = strlen(s1);
 	len2 = strlen(s2);
+	total = len1 + len2;
 
-	result = (char *)malloc(len1 + len2 + 1);
+	result = (char *)malloc((total + 1) * sizeof(char));
 	if (result == NULL)
 	{
 		return (NULL);
 	}
 	memcpy(result, s1, len1);
-	memcpy(result + len1, s2, len2 + 1);
+	memcpy(result + len1, s2, len2);
+	result[total] = '\0';
 	return (result);
 }
 
