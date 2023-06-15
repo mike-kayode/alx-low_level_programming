b6c18d1981b0037e78475432c5ac244e2198b1b9
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-15 13:48:09 -0700

    Malloc project

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,6 +12,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
+	unsigned int copy_size;
 
 	if (new_size == 0)
 	{
@@ -29,7 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	new_ptr = malloc(new_size);
 	if (new_ptr != NULL)
 	{
-		unsigned int copy_size = old_size < new_size ? old_size : new_size;
+		copy_size = old_size < new_size ? old_size : new_size;
 		memcpy(new_ptr, ptr, copy_size);
 		free(ptr);
 	}
