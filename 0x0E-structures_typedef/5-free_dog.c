e550d018ee636c95c57e62b83ed19fb44da1f02f
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-19 15:46:05 -0700

    structure project

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - function that end
+ * @d: array
+ * @dog_t: array
+ * Return: Success
+ */
+
 void free_dog(dog_t* d)
 {
 	if (d == NULL)
