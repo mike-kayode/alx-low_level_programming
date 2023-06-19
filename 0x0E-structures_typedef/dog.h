e68027a6425532f056eb0b2d425210c032cc5492
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-19 10:28:01 -0700

    Structure project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - short description
+ * @name: character
+ * @owner: character
+ * @age: integer
+ * Description: Dog attribute
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+/*
+ * dog_s - Typedef for dog
+ */
+
+typedef struct dog  dog_s;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_s *new_dog(char *name, float age, char *owner);
+void free_dog(dog_s *d);
+#endif
