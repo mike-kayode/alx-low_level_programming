25eb4268f3e89c1641090a420b4cabed3312d87b
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-19 16:02:31 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Function that Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
