065bd5bdf56005f423e6e7e0389c3f4bcfa22d82
mike-kayode <oluwakayodebright22@gmail.com>
2023-06-21 12:14:17 -0700

    Function pointer

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+/**
+ * op_add - function for sum
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of the integer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that substarct
+ * @a: first i nteger
+ * @b: second integer
+ * Return: substraction of the integer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that multiply
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplication pf the integer
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that divide
+ * @a: first integer
+ * @b: second integer
+ * Return: division pf the integer
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus function
+ * @a: first integer
+ * @b: secon integer
+ * Return: modulus of integers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
