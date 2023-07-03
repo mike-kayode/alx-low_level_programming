3e0bb34a3c9d7c17fa3a0bf269ff4c261fa2cc87
mike-kayode <oluwakayodebright22@gmail.com>
2023-07-03 14:30:01 -0700

    singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * print_list - function that print all element
+ * @h: pointer
+ * Return: Number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		if (current->str != NULL)
+		{
+			printf("%s\n", current->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
