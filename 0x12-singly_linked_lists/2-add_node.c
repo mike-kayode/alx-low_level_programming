fdbc1018f665ca1f238169c37e3419dd1b0782d9
mike-kayode <oluwakayodebright22@gmail.com>
2023-07-03 15:38:43 -0700

    singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,23 +13,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *new_str;
 
 	if (str == NULL)
 	return (NULL);
 	
 	new_node = (list_t *)malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 	return (NULL);
 
 	new_str = strdup(str);
-	if (new_str == NULL)
+	if (new_node->str == NULL)
 		free(new_node);
 	return (NULL);
 
-	new_node->str = new_str;
-	new_node->next = *head;
+	new_node->next = head;
 	*head = new_node;
 	return (new_node);
 }
