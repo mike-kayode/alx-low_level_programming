2aaed3b31240e809de5685e49199e35e089bbd37
mike-kayode <oluwakayodebright22@gmail.com>
2023-07-03 19:30:51 -0700

    singly linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,18 +1,5 @@
 #include "lists.h"
 
-/**
- * _strlen - find string length
- * @str: string
- * Return: length
- */
-int _strlen(const char *str)
-{
-	int len;
-
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	return (len);
-}
 
 /**
  * add_node_end - add node to end of linked list
@@ -20,32 +7,39 @@ int _strlen(const char *str)
  * @str: string
  * Return: address of new element
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *tmp;
+	list_t *new_node;
 
 	if (str == NULL)
 		return (NULL);
-	if (strdup(str) == NULL)
-		return (NULL);
-
+	
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
 		*head = new_node;
 	else
 	{
-		tmp = *head;
+		list_t *current = *head;
 
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = new_node;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
 	}
 
 	return (new_node);
