bd12f8b9f5c312afb80202fe3b23a78545e31310
mike-kayode <oluwakayodebright22@gmail.com>
2023-07-08 08:42:58 -0700

    Bit manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of a bit\
+ * @n: Pointer
+ * @index: Index of the bit
+ * Return: 1 if it work
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	mask = 1UL << index;
+	*n |= mask;
+	return (1);
+}
