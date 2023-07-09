1eee9b2c2ae2aee4d0fa6137a8eaeb564f34e634
mike-kayode <oluwakayodebright22@gmail.com>
2023-07-09 04:27:36 -0700

    Bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,12 +11,12 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor_result = n ^ m;
-        unsigned int count = 0;
-	
+	unsigned int count = 0;
+
 	while (xor_result != 0)
 	{
 		count += xor_result & 1;
 		xor_result >>= 1;
-        }
-        return (count);
+	}
+	return (count);
 }
