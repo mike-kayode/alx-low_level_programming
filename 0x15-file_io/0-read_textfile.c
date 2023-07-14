1b9513b1005e6be64fad81e8658de8cf5bb34215
mike-kayode <oluwakayodebright22@gmail.com>
2023-07-14 09:21:54 -0700

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -49,9 +49,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		fclose(file);
 		free(buffer);
 		return (0);
-	}
-
-	fclose(file);
+	}	fclose(file);
 	free(buffer);
 	return (bytesRead);
 }
